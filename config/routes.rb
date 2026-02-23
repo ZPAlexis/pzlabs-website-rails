7f2e79cdcd0fe1f9c6cb7076a6b3236a56fc3fd7
ZPAlexis <avfcoelho@gmail.com>
2026-02-23 13:56:08 -0300

    CI tests updated for remote repository

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get "projects/flappy_astronaut", to: "pages#flappy_astronaut", as: :pages_flappy_astronaut
   get "projects/trofy", to: "pages#trofy", as: :pages_trofy
   root "pages#index"
-  
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/.well-known/appspecific/com.chrome.devtools.json", to: proc { [ 200, {}, [ "{}" ] ] }
