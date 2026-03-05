254db9396eae3929d6e2a22d6e7fed991cfeb478
ZPAlexis <avfcoelho@gmail.com>
2026-03-05 19:42:05 -0300

    Updated routes.rb, application.rb, and application_controller.rb for i18n transition

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,18 @@ Rails.application.routes.draw do
     get  "metrics/coins", to: "events#metrics"
   end
 
-  get "about", to: "pages#about", as: :about
-  get "projects", to: "pages#projects", as: :projects
-  get "dev", to: "pages#dev", as: :dev
-  get "projects/born_survivor", to: "pages#born_survivor", as: :pages_born_survivor
-  get "projects/flappy_astronaut", to: "pages#flappy_astronaut", as: :pages_flappy_astronaut
-  get "projects/trofy", to: "pages#trofy", as: :pages_trofy
-  root "pages#index"
+  scope "(:locale)", locale: /en|pt/ do
+    get "about", to: "pages#about", as: :about
+    get "projects", to: "pages#projects", as: :projects
+    get "dev", to: "pages#dev", as: :dev
+
+    get "projects/born_survivor", to: "pages#born_survivor", as: :pages_born_survivor
+    get "projects/flappy_astronaut", to: "pages#flappy_astronaut", as: :pages_flappy_astronaut
+    get "projects/trofy", to: "pages#trofy", as: :pages_trofy
+
+    root "pages#index"
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
-
   get "/.well-known/appspecific/com.chrome.devtools.json", to: proc { [ 200, {}, [ "{}" ] ] }
 end
