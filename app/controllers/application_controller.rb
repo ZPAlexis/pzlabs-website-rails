7c30a9d7acdf110f384ae517cef6e1099c0bfb5e
ZPAlexis <avfcoelho@gmail.com>
2026-03-05 19:42:05 -0300

    Updated routes.rb, application.rb, and application_controller.rb for i18n transition

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    parsed_locale = params[:locale]
+    I18n.locale = I18n.available_locales.include?(parsed_locale&.to_sym) ? parsed_locale : I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
